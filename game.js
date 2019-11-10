57d9c9200762494ed995fb9f2d0967594d540801
malekmalek1992 <56638897+malekmalek1992@users.noreply.github.com>
2019-11-10 16:43:21 +0100

    Update game.js

--- game.js
+++ game.js
@@ -6,8 +6,7 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-
-$(document).keypress(function() {
+ $(document).on("click touchstart",function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
